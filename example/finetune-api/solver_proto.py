31e77616223d4c34752119a58864efb2fc0acc11
Tao Wei <taowei@buffalo.edu>
2016-03-06 11:07:38 +0800

    update proto for train_imagenet.py

--- example/finetune-api/solver_proto.py
+++ example/finetune-api/solver_proto.py
@@ -2,23 +2,23 @@ import argparse
 import yaml
 
 def proto_parser():
-    parser = argparse.ArgumentParser(description='train an image classifer on mnist')
-#    parser.add_argument('--network', type=str, default='mlp',
-#                        choices = ['mlp', 'lenet'],
+    parser = argparse.ArgumentParser(description='train an image classifer on imagenet')
+#    parser.add_argument('--network', type=str, default='inception-bn',
+#                        choices = ['alexnet', 'vgg', 'googlenet', 'inception-bn', 'inception-bn-full', 'inception-v3', 'vgg16'],
 #                        help = 'the cnn to use')
-    parser.add_argument('--data-dir', type=str, default='mnist/',
+    parser.add_argument('--data-dir', type=str,
                         help='the input data directory')
-#    parser.add_argument('--gpus', type=str,
-#                        help='the gpus will be used, e.g "0,1,2,3"')
-    parser.add_argument('--num-examples', type=int, default=60000,
-                        help='the number of training examples')
-    parser.add_argument('--batch-size', type=int, default=128,
-                        help='the batch size')
-    parser.add_argument('--lr', type=float, default=.1,
-                        help='the initial learning rate')
     parser.add_argument('--model-prefix', type=str,
                         help='the prefix of the model to load/save')
-    parser.add_argument('--num-epochs', type=int, default=10,
+    parser.add_argument('--lr', type=float, default=.01,
+                        help='the initial learning rate')
+    parser.add_argument('--lr-factor', type=float, default=1,
+                        help='times the lr with a factor for every lr-factor-epoch epoch')
+    parser.add_argument('--lr-factor-epoch', type=float, default=1,
+                        help='the number of epoch to factor the lr, could be .5')
+    parser.add_argument('--clip-gradient', type=float, default=5.,
+                        help='clip min/max gradient to prevent extreme value')
+    parser.add_argument('--num-epochs', type=int, default=20,
                         help='the number of training epochs')
 #    parser.add_argument('--load-epoch', type=int,
 #                        help="load the model on an epoch using the model-prefix")
@@ -26,16 +26,28 @@ def proto_parser():
 #                        help="finetune from model")
     parser.add_argument('--finetune-lr-scale', type=float, default=10,
                         help="finetune layer lr_scale")
-    parser.add_argument('--num-classes', type=int, default=10,
-                        help='the number of classes')
-#    parser.add_argument('--dataset', type=str, default='mnist',
-#                        help='dataset')
+    parser.add_argument('--batch-size', type=int, default=32,
+                        help='the batch size')
+#    parser.add_argument('--gpus', type=str,
+#                        help='the gpus will be used, e.g "0,1,2,3"')
     parser.add_argument('--kv-store', type=str, default='local',
                         help='the kvstore type')
-    parser.add_argument('--lr-factor', type=float, default=1,
-                        help='times the lr with a factor for every lr-factor-epoch epoch')
-    parser.add_argument('--lr-factor-epoch', type=float, default=1,
-                        help='the number of epoch to factor the lr, could be .5')
+    parser.add_argument('--num-examples', type=int, default=1281167,
+                        help='the number of training examples')
+    parser.add_argument('--num-classes', type=int, default=1000,
+                        help='the number of classes')
+#    parser.add_argument('--dataset', type=str, default='imagenet',
+#                        help='dataset')
+    parser.add_argument('--log-file', type=str, 
+    		    help='the name of log file')
+    parser.add_argument('--log-dir', type=str, default="/tmp/",
+                        help='directory of the log file')
+    parser.add_argument('--train-dataset', type=str, default="train.rec",
+                        help='train dataset name')
+    parser.add_argument('--val-dataset', type=str, default="val.rec",
+                        help="validation dataset name")
+    parser.add_argument('--data-shape', type=int, default=224,
+                        help='set image\'s shape')
     return parser
     
 def dict_to_arg_list(params):
