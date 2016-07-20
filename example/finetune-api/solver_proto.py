6413df53a9669926fedbf62bfe75e771cec3af0a
Tao Wei <taowei@buffalo.edu>
2016-07-20 14:56:46 +0800

    initializer support

--- example/finetune-api/solver_proto.py
+++ example/finetune-api/solver_proto.py
@@ -68,6 +68,8 @@ def proto_parser():
                         help = 'the cnn to use')
     parser.add_argument('--network-kwargs', type=str, default='{}',
                         help = 'network symbol kwargs')
+    parser.add_argument('--initializer', type=str, default='msra', choices=['xavier', 'msra', 'old'],
+                        help = 'the initializer to use for weights')
 #    parser.add_argument('--gpus', type=str,
 #                        help='the gpus will be used, e.g "0,1,2,3"')
     parser.add_argument('--kv-store', type=str, default='local',
