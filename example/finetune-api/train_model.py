6d2fb3a86d1cc9954458e60283eff91cc2a9b1b8
Tao Wei <taowei@buffalo.edu>
2016-07-23 16:57:52 +0800

    initializer old->default

--- example/finetune-api/train_model.py
+++ example/finetune-api/train_model.py
@@ -116,7 +116,7 @@ def fit(args, network, data_loader):
         initializer = mx.init.Xavier(factor_type="in", magnitude=3.0)
     elif args.initializer == 'msra':
         initializer = mx.init.Xavier(factor_type="in", rnd_type="gaussian", magnitude=2)
-    elif args.initializer == 'old':
+    elif args.initializer == 'default':
         initializer = mx.init.Xavier(factor_type="in", magnitude=2.34)
     else:
         raise ValueError('Invalid initializer: %s' % args.initializer)
