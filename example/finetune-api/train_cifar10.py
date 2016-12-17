d0c3e4d01b01e90d2575ef797c931050961e1d02
Tao Wei <taowei@buffalo.edu>
2016-12-18 01:33:29 +0800

    merge train_cifar10.py and train_cifar100.py

--- example/finetune-api/train_cifar10.py
+++ example/finetune-api/train_cifar10.py
@@ -2,10 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import find_mxnet
-import mxnet as mx
 import logging
 import argparse
-import os
 import train_model
 
 from dataiter import NDArraySimpleAugmentationIter
@@ -60,6 +58,48 @@ def load_CIFAR10(root_dir='cifar-10-batches-py', return_classes=False):
         classes = meta['label_names']
         return Xtr, Ytr, Xte, Yte, classes
 
+def load_CIFAR100(root_dir='cifar-100-python', return_classes=False):
+    '''Load CIFAR 100 dataset.
+
+    Returns
+    -------
+    Xtr : ndarray
+        50000 32x32 RGB uint8 images.
+    Ytr : ndarray
+        Train labels.
+    Xte : ndarray
+        10000 32x32 RGB uint8 images.
+    Yte : ndarray
+        Test labels.
+    classes : list of string (if return_classes=True)
+        Image labels.
+    '''
+
+    import cPickle
+    import numpy as np
+    import os
+
+    def unpickle(filename):
+        with open(filename, 'rb') as f:
+            dict = cPickle.load(f)
+        return dict
+
+    def load_CIFAR_batch(filename):
+        train = unpickle(filename)
+        X = train['data'].reshape((-1,3,32,32)).transpose(0,2,3,1) # 50000 32x32 RGB unit8 images
+        y = np.array(train['fine_labels'], dtype=np.int64)
+        return X, y
+
+    Xtr, Ytr = load_CIFAR_batch(os.path.join(root_dir, 'train'))
+    Xte, Yte = load_CIFAR_batch(os.path.join(root_dir, 'test'))
+
+    if not return_classes:
+        return Xtr, Ytr, Xte, Yte
+    else:
+        meta = unpickle(os.path.join(root_dir, 'meta'))
+        classes = meta['fine_label_names']
+        return Xtr, Ytr, Xte, Yte, classes
+
 # don't use -n and -s, which are resevered for the distributed training
 def parse_args():
     parser = argparse.ArgumentParser(description='train an image classifer on mnist')
@@ -89,7 +129,14 @@ if __name__ == '__main__':
     # data
     def get_iterator(args, kv):
         data_shape = tuple([int(i) for i in args.data_shape.split(',')])
-        Xtr, Ytr, Xte, Yte = load_CIFAR10(args.data_dir)
+        if args.dataset == 'cifar10':
+            assert args.num_classes == 10
+            Xtr, Ytr, Xte, Yte = load_CIFAR10(args.data_dir)
+        elif args.dataset == 'cifar100':
+            assert args.num_classes == 100
+            Xtr, Ytr, Xte, Yte = load_CIFAR100(args.data_dir)
+        else:
+            raise ValueError('Invalid dataset: %s', args.dataset)
         mean_values = [float(v) for v in args.mean_values.split(',')] if args.mean_values else None
         scale = None if args.scale == 1.0 else args.scale
 
@@ -97,7 +144,7 @@ if __name__ == '__main__':
             label = Ytr,
             batch_size = args.batch_size,
             shuffle=True, shuffle_on_reset=True,
-            pad=args.pad, random_mirror=True, data_shape=data_shape, random_crop=True, 
+            pad=args.pad, random_mirror=True, data_shape=data_shape, random_crop=True,
             mean_values=mean_values, scale=scale, last_batch_handle='discard')
 
         if args.val_dataset:
@@ -105,7 +152,7 @@ if __name__ == '__main__':
                 label = Yte,
                 batch_size = args.batch_size,
                 shuffle=False, shuffle_on_reset=False,
-                pad=args.pad, random_mirror=False, data_shape=data_shape, random_crop=False, 
+                pad=args.pad, random_mirror=False, data_shape=data_shape, random_crop=False,
                 mean_values=mean_values, scale=scale, last_batch_handle='discard')
         else:
             logging.info('Valication dataset is not provided, hence evaluation is disabled.')
